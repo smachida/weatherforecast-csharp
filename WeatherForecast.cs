6f58b1666c3a12c93e73a9cf4f34117154a046de
Shuichi Machida <shuichi.machida@gmail.com>
2023-03-09 16:42:03 +0900

    updated

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,12 +1,14 @@
-namespace weatherforecast-csharp;
+namespace weatherforecast-csharp {
 
-public class WeatherForecast
-{
-    public DateTime Date { get; set; }
+  public class WeatherForecast
+  {
+      public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+      public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+      public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+      public string? Summary { get; set; } 
+  }
 }
+
